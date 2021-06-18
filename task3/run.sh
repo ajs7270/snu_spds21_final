453c00c7b2a8958507a1f63362915c534cccd34e
jisuAn <ajs7270@naver.com>
2021-06-18 11:30:13 +0000

    finish task3

--- task3/run.sh
+++ task3/run.sh
@@ -12,4 +12,4 @@ echo ${USER}
 eval "$(conda shell.bash hook)"
 conda activate spds078              # Activate your conda environment
 
-srun python main.py --epochs 30
+srun python main.py --epochs 10
